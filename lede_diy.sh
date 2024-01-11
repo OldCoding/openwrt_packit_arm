822c2477c5511751747fddcc4e3a412eb8c57cf3
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-01-11 16:11:07 +0800

    Create lede_diy.sh

---
+++ lede_diy.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+svn_export() {
+	# 参数1是分支名, 参数2是子目录, 参数3是目标目录, 参数4仓库地址
+	trap 'rm -rf "$TMP_DIR"' 0 1 2 3
+	TMP_DIR="$(mktemp -d)" || exit 1
+	[ -d "$3" ] || mkdir -p "$3"
+	TGT_DIR="$(cd "$3"; pwd)"
+	cd "$TMP_DIR" && \
+	git init >/dev/null 2>&1 && \
+	git remote add -f origin "$4" >/dev/null 2>&1 && \
+	git checkout "remotes/origin/$1" -- "$2" && \
+	cd "$2" && cp -a . "$TGT_DIR/"
+}
+
+git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot
+git clone --depth 1 https://github.com/gngpp/luci-theme-design
+git clone --depth 1 https://github.com/sbwml/luci-app-alist
+git clone --depth 1 https://github.com/sirpdboy/netspeedtest
+git clone --depth 1 https://github.com/lxl6125/openwrt-qbittorrent-enhanced
+git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush
+git clone --depth 1 https://github.com/fw876/helloworld
+svn_export "main" "luci-app-passwall" "openwrt/package" "https://github.com/xiaorouji/openwrt-passwall"
+svn_export "main" "luci-app-passwall2" "openwrt/package" "https://github.com/xiaorouji/openwrt-passwall2"
+svn_export "master" "luci-app-diskman" "openwrt/package" "https://github.com/kiddin9/openwrt-packages"
+svn_export "main" "luci-app-adguardhome" "openwrt/package" "https://github.com/sirpdboy/sirpdboy-package"
+svn_export "main" "luci-app-filebrowser" "openwrt/package" "https://github.com/Lienol/openwrt-package"
+svn_export "main" "luci-app-amlogic" "openwrt/package" "https://github.com/ophub/luci-app-amlogic"
+svn_export "main" "openwrt/luci-app-thunder" "openwrt/package" "https://github.com/gngpp/nas-xunlei"
+svn_export "main" "openwrt/thunder" "openwrt/package" "https://github.com/gngpp/nas-xunlei"
+svn_export "dev" "luci-app-openclash" "openwrt/package" "https://github.com/vernesong/OpenClash"
+
+cd openwrt
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+cd package
+sed -i "s/OpenWrt /Wing build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" lean/default-settings/files/zzz-default-settings
+sed -i "/firewall\.user/d" lean/default-settings/files/zzz-default-settings
+sed -i "s|https.*/amlogic-s9xxx-openwrt|https://github.com/OldCoding/openwrt_packit_arm|g" luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|s9xxx_lede|${{ env.PRODUCT_NAME }}|g" luci-app-amlogic/root/etc/config/amlogic
+curl -sfL -o ./luci-app-passwall/root/usr/share/passwall/rules/gfwlist https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt
+chmod 755 ./luci-app-adguardhome/root/etc/init.d/AdGuardHome
+cd ./luci-app-openclash/root/etc/openclash
+CORE_VER=https://github.com/vernesong/OpenClash/raw/core/dev/core_version
+CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-arm64
+CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-arm64.tar.gz
+CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-arm64.tar.gz
+TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+curl -sfL -o ./Country.mmdb https://github.com/alecthw/mmdb_china_ip_list/raw/release/Country.mmdb
+curl -sfL -o ./GeoSite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
+curl -sfL -o ./GeoIP.dat https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+mkdir ./core && cd ./core
+curl -sfL -o ./tun.gz "$CORE_TUN"-"$TUN_VER".gz && gzip -d ./tun.gz && mv ./tun ./clash_tun
+curl -sfL -o ./meta.tar.gz "$CORE_MATE" && tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
+curl -sfL -o ./dev.tar.gz "$CORE_DEV" && tar -zxf ./dev.tar.gz
+chmod +x ./clash* ; rm -rf ./*.gz
+cd $GITHUB_WORKSPACE/openwrt && cd feeds/luci/applications/luci-app-wrtbwmon
+sed -i 's/ selected=\"selected\"//g' ./luasrc/view/wrtbwmon/wrtbwmon.htm && sed -i 's/\"1\"/\"1\" selected=\"selected\"/g' ./luasrc/view/wrtbwmon/wrtbwmon.htm
+sed -i 's/interval: 5/interval: 1/g' ./htdocs/luci-static/wrtbwmon/wrtbwmon.js
