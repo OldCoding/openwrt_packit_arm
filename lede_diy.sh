ef6cbc8cfc32073bbfa778300bbd60b26e2d1a2c
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-02-27 19:23:23 +0800

    Update lede_diy.sh

--- lede_diy.sh
+++ lede_diy.sh
@@ -59,8 +59,6 @@ cat feeds/packages/utils/xfsprogs/Makefile
 # 替换argon主题
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # 个性化设置
 cd package
 sed -i "s/OpenWrt /Wing build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" lean/default-settings/files/zzz-default-settings
