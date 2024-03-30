a8f61b778da991a7424d8d8a96130ad9f6ad41a3
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-03-30 18:15:04 +0800

    Update lede_diy.sh

--- lede_diy.sh
+++ lede_diy.sh
@@ -21,7 +21,7 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/net/v2ray-geodata
-#git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 # 下载插件
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot feeds/luci/applications/luci-app-pushbot
 git clone --depth 1 https://github.com/gngpp/luci-theme-design package/luci-theme-design
