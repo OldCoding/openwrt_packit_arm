945b02812e477911a7fcb7a38d87bd9509e062c1
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-06-08 14:57:49 +0800

    Update op_diy.sh

--- op_diy.sh
+++ op_diy.sh
@@ -37,6 +37,7 @@ git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages packa
 git clone --depth 1 https://github.com/fw876/helloworld package/helloworld
 git clone --depth 1 https://github.com/chenmozhijin/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
 svn_export "main" "luci-app-passwall" "package/luci-app-passwall" "https://github.com/xiaorouji/openwrt-passwall"
 svn_export "main" "luci-app-passwall2" "package/luci-app-passwall2" "https://github.com/xiaorouji/openwrt-passwall2"
 svn_export "main" "luci-app-amlogic" "package/luci-app-amlogic" "https://github.com/ophub/luci-app-amlogic"
