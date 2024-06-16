cd1a497ba6ce97041075c54a64545999abf62b4c
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-06-16 18:22:33 +0800

    Update op_diy.sh

--- op_diy.sh
+++ op_diy.sh
@@ -74,7 +74,7 @@ svn_export "v5" "mosdns" "package/mosdns" "https://github.com/sbwml/luci-app-mos
 svn_export "v5" "v2dat" "package/v2dat" "https://github.com/sbwml/luci-app-mosdns"
 svn_export "master" "package/emortal" "package/emortal" "https://github.com/immortalwrt/immortalwrt"
 
-svn_export "openwrt-23.05" "package/libs/mbedtls" "package/libs/mbedtls" "https://github.com/immortalwrt/immortalwrt"
+svn_export "openwrt-23.05" "package/libs/mbedtls" "package/libs/mbedtls" "https://github.com/openwrt/openwrt"
 
 # turboacc 补丁
 #curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash -x add_turboacc.sh
