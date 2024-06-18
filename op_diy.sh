85eaf0cbf1a44329363ebb7e39832cfc00e26e5b
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-06-18 18:46:01 +0800

    Update op_diy.sh

--- op_diy.sh
+++ op_diy.sh
@@ -12,9 +12,9 @@ svn_export() {
 	rm -rf "$TMP_DIR"
 }
 
-rm -rf package/libs/mbedtls
-rm -rf package/libs/ustream-ssl
-rm -rf package/libs/uclient
+#rm -rf package/libs/mbedtls
+#rm -rf package/libs/ustream-ssl
+#rm -rf package/libs/uclient
 
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
@@ -76,9 +76,9 @@ svn_export "v5" "mosdns" "package/mosdns" "https://github.com/sbwml/luci-app-mos
 svn_export "v5" "v2dat" "package/v2dat" "https://github.com/sbwml/luci-app-mosdns"
 svn_export "master" "package/emortal" "package/emortal" "https://github.com/immortalwrt/immortalwrt"
 
-svn_export "master" "package/libs/mbedtls" "package/libs/mbedtls" "https://github.com/coolsnowwolf/lede"
-svn_export "master" "package/libs/ustream-ssl" "package/libs/ustream-ssl" "https://github.com/coolsnowwolf/lede"
-svn_export "master" "package/libs/uclient" "package/libs/uclient" "https://github.com/coolsnowwolf/lede"
+#svn_export "master" "package/libs/mbedtls" "package/libs/mbedtls" "https://github.com/coolsnowwolf/lede"
+#svn_export "master" "package/libs/ustream-ssl" "package/libs/ustream-ssl" "https://github.com/coolsnowwolf/lede"
+#svn_export "master" "package/libs/uclient" "package/libs/uclient" "https://github.com/coolsnowwolf/lede"
 
 # turboacc 补丁
 #curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash -x add_turboacc.sh
