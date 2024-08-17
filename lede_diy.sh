380821214c801dc3abaaabbdf8c2e2bce3512e98
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-08-17 10:54:34 +0800

    fix luci-app-filebrowser

--- lede_diy.sh
+++ lede_diy.sh
@@ -81,7 +81,7 @@ git clone --depth 1 https://github.com/sbwml/autocore-arm package/lean/autocore
 
 # 调整菜单位置
 sed -i "s|services|nas|g" feeds/luci/applications/luci-app-aria2/root/usr/share/luci/menu.d/luci-app-aria2.json
-sed -i "s|services|nas|g" feeds/luci/applications/luci-app-filebrowser-go/root/usr/share/luci/menu.d/luci-app-filebrowser-go.json
+sed -i "s|services|nas|g" feeds/luci/applications/luci-app-filebrowser-go/root/usr/share/luci/menu.d/luci-app-filebrowser.json
 sed -i "s|services|nas|g" feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
 sed -i "s|services|nas|g" feeds/luci/applications/luci-app-transmission/root/usr/share/luci/menu.d/luci-app-transmission.json
 sed -i "s|services|nas|g" feeds/luci/applications/luci-app-hd-idle/root/usr/share/luci/menu.d/luci-app-hd-idle.json
