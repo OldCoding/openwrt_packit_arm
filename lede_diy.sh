8d96cdd50a96b1e66a1bb9c2e054c5338fae77b6
OldCoding <85047508+OldCoding@users.noreply.github.com>
2025-03-13 21:58:07 +0800

    docker

--- lede_diy.sh
+++ lede_diy.sh
@@ -43,14 +43,15 @@ rm -rf feeds/packages/net/lucky
 rm -rf feeds/packages/utils/docker-compose
 rm -rf feeds/packages/utils/docker
 rm -rf feeds/packages/utils/dockerd
+rm -rf feeds/packages/utils/containerd
+rm -rf feeds/packages/utils/runc
+rm -rf feeds/packages/utils/btrfs-progs
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/net/aria2
 rm -rf feeds/packages/lang/php8
 rm -rf feeds/packages/net/wget
 rm -rf feeds/packages/utils/ttyd
-rm -rf feeds/packages/utils/runc
-rm -rf feeds/packages/utils/containerd
 rm -rf feeds/packages/utils/coremark
 rm -rf feeds/packages/libs/tiff
 rm -rf feeds/packages/libs/libdht
@@ -113,6 +114,7 @@ svn_export "master" "utils/docker" "feeds/packages/utils/docker" "https://github
 svn_export "master" "utils/dockerd" "feeds/packages/utils/dockerd" "https://github.com/immortalwrt/packages"
 svn_export "master" "utils/containerd" "feeds/packages/utils/containerd" "https://github.com/immortalwrt/packages"
 svn_export "master" "utils/runc" "feeds/packages/utils/runc" "https://github.com/immortalwrt/packages"
+svn_export "master" "utils/btrfs-progs" "feeds/packages/utils/btrfs-progs" "https://github.com/immortalwrt/packages"
 svn_export "master" "lang/php8" "feeds/packages/lang/php8" "https://github.com/immortalwrt/packages"
 svn_export "master" "utils/ttyd" "feeds/packages/utils/ttyd" "https://github.com/immortalwrt/packages"
 svn_export "main" "luci-app-amlogic" "package/luci-app-amlogic" "https://github.com/ophub/luci-app-amlogic"
