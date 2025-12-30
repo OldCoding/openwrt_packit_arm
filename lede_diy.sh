f88a8493b495b7a0361e97eb73749960d186e66b
OldCoding <qq1049066154@gmail.com>
2025-12-30 23:08:41 +0800

    ddns

--- lede_diy.sh
+++ lede_diy.sh
@@ -14,6 +14,8 @@ svn_export() {
 
 # 删除冲突软件和依赖
 rm -rf tools/libdeflate
+rm -rf package/lean/ddns-scripts_aliyun
+rm -rf package/lean/ddns-scripts_dnspod
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/luci/applications/luci-app-aria2
 rm -rf feeds/luci/applications/luci-app-cloudflared
