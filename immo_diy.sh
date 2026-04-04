07723410c33fa2d009b66afc6b236d1dbb70b079
OldCoding <qq1049066154@gmail.com>
2026-04-04 10:22:43 +0800

    turboacc

--- immo_diy.sh
+++ immo_diy.sh
@@ -83,7 +83,7 @@ mv ./package/adguardhome/* ./package/ && rm -rf ./package/adguardhome
 mv ./package/netdata/luci-app-netdata ./package/ && rm -rf ./package/netdata
 
 # turboacc 补丁
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh 25.12
 
 
 # 调整菜单位置
